aafd9e6972fc8826e033007c50113a65a48e74c8
Camilo Niño <camiloreact12@gmail.com>
2025-11-08 23:24:11 -0500

    arreglo errores

--- database/migrations/1762537434975_clients.ts
+++ database/migrations/1762537434975_clients.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-      table.string("id_user").notNullable(); // FK a users (si existe)
+      table.string("id_user").notNullable(); 
       table.integer("phone").notNullable();
       table.string("city", 100).notNullable();
       table.integer("cc").notNullable().unique();
--- database/migrations/1762541020473_administrators.ts
+++ database/migrations/1762541020473_administrators.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-      table.integer("id_user"); // referencia a usuario en otro sistema o MongoDB
+      table.string("id_user").notNullable(); 
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
