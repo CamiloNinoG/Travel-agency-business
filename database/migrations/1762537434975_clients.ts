3ccd50c717cd786e457c8433a50ccd5c013ba620
Camilo Niño <camiloreact12@gmail.com>
2025-11-09 16:41:15 -0500

    clientes null

--- database/migrations/1762537434975_clients.ts
+++ database/migrations/1762537434975_clients.ts
@@ -1,25 +1,25 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'clients'
+  protected tableName = "clients";
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-      table.string("id_user").notNullable(); 
-      table.integer("phone").notNullable();
-      table.string("city", 100).notNullable();
-      table.integer("cc").notNullable().unique();
+      table.string("id_user").notNullable();
+      table.integer("phone").nullable();
+      table.string("city", 100).nullable();
+      table.integer("cc").unique().nullable();
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
-    })
+      table.timestamp("created_at", { useTz: true });
+      table.timestamp("updated_at", { useTz: true });
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
